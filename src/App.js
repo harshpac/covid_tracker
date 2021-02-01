98b55b4229466a5831753a9de817dbdefe8caa72
harshpac <harshpachauri3001@gmail.com>
2021-02-01 23:41:15 +0530

    taking data from api and using material ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import styles from './App.module.css';
 import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
-function App() {
-  return (
-    <div className="App">
-        <Cards></Cards>
-        <Chart></Chart>
-        <CountryPicker></CountryPicker>
-    </div>
-  );
+import {fetchData} from './api';
+
+class App extends Component {
+
+    state = {
+        data: {},  
+    }
+
+   async componentDidMount() {
+        const data = await fetchData();
+        console.log('did mount');
+        this.setState({data: data});
+    }
+
+  render(){
+    const {data} = this.state;
+    console.log(data);
+    return (
+        
+        <div className={styles.container}>
+            <Cards data = {data}></Cards>
+            <Chart></Chart>
+            <CountryPicker></CountryPicker>
+        </div>
+      );
+  }
+ 
 }
 
 export default App;
